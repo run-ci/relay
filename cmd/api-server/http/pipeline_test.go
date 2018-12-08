9e2efe85900779f14087973d72d6a95cbbc59dc3
Hugo Torres <hugo.torres1993@gmail.com>
2018-12-08 11:27:31 -0500

    Add test for GET /pipelines/{id}

--- cmd/api-server/http/pipeline_test.go
+++ cmd/api-server/http/pipeline_test.go
@@ -42,28 +42,28 @@ func (st *memStore) seedPipelines() {
 		projectID    int
 	}{
 		{
-			id:           0,
+			id:           1,
 			name:         "default",
 			success:      true,
 			remoteURL:    "https://github.com/run-ci/relay.git",
 			remoteBranch: "master",
-			projectID:    0,
+			projectID:    1,
 		},
 		{
-			id:           1,
+			id:           2,
 			name:         "docker",
 			success:      true,
 			remoteURL:    "https://github.com/run-ci/relay.git",
 			remoteBranch: "master",
-			projectID:    0,
+			projectID:    1,
 		},
 		{
-			id:           2,
+			id:           3,
 			name:         "default",
 			success:      false,
 			remoteURL:    "https://github.com/run-ci/run.git",
 			remoteBranch: "master",
-			projectID:    1,
+			projectID:    2,
 		},
 	}
 
@@ -136,3 +136,81 @@ func TestGetPipelines(t *testing.T) {
 
 	// TODO: test pipelines that are returned
 }
+
+func TestGetPipeline(t *testing.T) {
+	st := &memStore{
+		pipelinedb: make(map[int]store.Pipeline),
+	}
+	st.seedPipelines()
+
+	srv := NewServer(":9001", make(chan []byte), st)
+
+	test := struct {
+		input    int
+		expected store.Pipeline
+		actual   store.Pipeline
+	}{
+		input:    1,
+		expected: st.pipelinedb[1],
+		actual:   store.Pipeline{},
+	}
+
+	r := mux.NewRouter()
+	r.Handle("/pipelines/{id}", chain(srv.handleGetPipeline, setRequestID))
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	requrl := fmt.Sprintf("%v/pipelines/%v", ts.URL, test.input)
+	req, err := http.NewRequest(http.MethodGet, requrl, nil)
+	if err != nil {
+		t.Fatalf("error creating http request for test: %v", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("error executing test against test server: %v", err)
+	}
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("got error reading response body: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected status code %v, got %v", http.StatusOK, resp.StatusCode)
+	}
+
+	err = json.Unmarshal(buf, &test.actual)
+	if err != nil {
+		t.Fatalf("got error unmarshaling pipeline: %v", err)
+	}
+
+	if test.expected.ID != test.actual.ID {
+		t.Fatalf("expected id %v, got %v", test.expected.ID, test.actual.ID)
+	}
+
+	if test.expected.Name != test.actual.Name {
+		t.Fatalf("expected Name %v, got %v", test.expected.Name, test.actual.Name)
+	}
+
+	if *test.expected.Success != *test.actual.Success {
+		t.Fatalf("expected Success %v, got %v", *test.expected.Success, *test.actual.Success)
+	}
+
+	if test.expected.ProjectID != test.actual.ProjectID {
+		t.Fatalf("expected ProjectID %v, got %v", test.expected.ProjectID, test.actual.ProjectID)
+	}
+
+	if test.expected.GitRemote.URL != test.actual.GitRemote.URL {
+		t.Fatalf("expected GitRemote.URL %v, got %v", test.expected.GitRemote.URL, test.actual.GitRemote.URL)
+	}
+
+	if test.expected.GitRemote.Branch != test.actual.GitRemote.Branch {
+		t.Fatalf("expected GitRemote.Branch %v, got %v", test.expected.GitRemote.Branch, test.actual.GitRemote.Branch)
+	}
+
+}
+
+// TODO: test pipeline not found returns 404
