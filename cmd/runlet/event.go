ff0f9144670b31e0b99c7f38e54486c99b83f794
Hugo Torres <hugo.torres1993@gmail.com>
2019-01-07 00:22:05 -0500

    Fix broken communication from  poller to runlet

--- cmd/runlet/event.go
+++ cmd/runlet/event.go
@@ -14,6 +14,17 @@ type Event struct {
 
 // Step is a grouping of tasks that can be run in parallel.
 type Step struct {
-	Name  string     `json:"name"`
-	Tasks []run.Task `json:"tasks"`
+	Name  string `json:"name"`
+	Tasks []Task `json:"tasks"`
+}
+
+// Task is a run task.
+type Task struct {
+	run.Task
+
+	// Shadowing this because an argument in a normal run task
+	// isn't the value of the actual argument, but a set of
+	// sources where that value can be obtained. In a pipeline
+	// run, it's necessary to have the actual value.
+	Arguments map[string]interface{} `json:"arguments"`
 }
